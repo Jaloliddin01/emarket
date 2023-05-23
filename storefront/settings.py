19d89b1e4e66851952c16cbe965fbf6bbf5ea130
Jaloliddin <d.murodullaev@easybooking.uz>
2023-05-23 13:46:08 +0500

    database settings updated

--- storefront/settings.py
+++ storefront/settings.py
@@ -84,11 +84,8 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'storefront',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'MyPassword'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
